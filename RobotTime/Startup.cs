590fd3a6eb16a76f5c01f8f62762f09bf7e6cac7
Kelvin Snepvangers <kelvinsnepvangers@gmail.com>
2019-11-05 17:50:38 +0100

    Cleaned up the extra projext

--- RobotTime/Startup.cs
+++ RobotTime/Startup.cs
@@ -5,11 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace RobotTime
+namespace WebApplication1
 {
     public class Startup
     {
@@ -23,7 +24,8 @@ namespace RobotTime
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddDbContext<MyContext>(options => options.UseSqlServer(@"Server=tcp:robottimedbserver.database.windows.net,1433;Initial Catalog=RobotTime_db;Persist Security Info=False;User ID=klokkenman1;Password=tLB5F5!$E8sPeNtIH^gL;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,21 +37,22 @@ namespace RobotTime
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
